432ee519fd46c45e9175654a98509fbe50ac7254
tangyc <874500641@qq.com>
2017-03-08 19:26:55 +0800

    引导动画和主界面的完成

--- app/src/main/java/com/tang/zgan/home/activity/MainActivity.java
+++ app/src/main/java/com/tang/zgan/home/activity/MainActivity.java
@@ -3,8 +3,14 @@ package com.tang.zgan.home.activity;
 import android.app.ProgressDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +18,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -29,7 +36,7 @@ import com.tang.zgan.home.view.MeiziView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MeiziView ,AndroidView,OnItemListener{
+public class MainActivity extends AppCompatActivity implements MeiziView ,AndroidView,OnItemListener, NavigationView.OnNavigationItemSelectedListener {
 
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
@@ -44,10 +51,28 @@ public class MainActivity extends AppCompatActivity implements MeiziView ,Androi
     private ProgressDialog dialog;
     private RecyclerView mRvMeizi;
     private RecyclerView mRvAndroid;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    return true;
+                case R.id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
             int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -64,6 +89,19 @@ public class MainActivity extends AppCompatActivity implements MeiziView ,Androi
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mTabLayout = (TabLayout) findViewById(R.id.tablayout);
         mIv= (ImageView) findViewById(R.id.iv);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         mInflater = LayoutInflater.from(this);
         viewAndroid = mInflater.inflate(R.layout.item_view, null);
         viewMeizi = mInflater.inflate(R.layout.item_meizi, null);
@@ -98,7 +136,15 @@ public class MainActivity extends AppCompatActivity implements MeiziView ,Androi
         androidPresenter.getAndroid(30,1);
 
     }
-
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
     @Override
     public void showLoading() {
         dialog.show();
@@ -132,5 +178,29 @@ public class MainActivity extends AppCompatActivity implements MeiziView ,Androi
     @Override
     public void onItemClick(View view, Object obj) {
 
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
